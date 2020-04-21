de888d98e93494a5493cc055cf4e0c6a1e8440a1
rohitpractise <rohitgulati8@gmail.com>
2020-04-22 03:46:52 +0800

    Updates

---
+++ precache-manifest.757cf2d292783e865e5ec4fbe3ff491a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfccca035b1d97b6900b67c75f8de3f5",
+    "url": "/codepractise/index.html"
+  },
+  {
+    "revision": "19d23069e709f586655b",
+    "url": "/codepractise/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "696c7b255a0ff540d07d",
+    "url": "/codepractise/static/css/main.b3974bb7.chunk.css"
+  },
+  {
+    "revision": "19d23069e709f586655b",
+    "url": "/codepractise/static/js/2.2ca643fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepractise/static/js/2.2ca643fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696c7b255a0ff540d07d",
+    "url": "/codepractise/static/js/main.b144888b.chunk.js"
+  },
+  {
+    "revision": "6f5f3d1a26e44cbda544",
+    "url": "/codepractise/static/js/runtime-main.700d5533.js"
+  }
+]);
